442b0b4631bc811aea53960aa334f529e77fb2de
Adam Romano <manoadamro.imp@gmail.com>
2018-10-16 19:03:44 +0100

    added tox.ini coveragerc and placeholder test

---
+++ setup.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+import setuptools
+
+
+NAME = 'flask-jwt'
+VERSION = '0.0.0'
+
+
+REQUIRES = [
+
+]
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    install_requires=REQUIRES,
+    packages=setuptools.find_packages()
+)
---
+++ tests/test_placeholder.py
@@ -0,0 +1,7 @@
+import unittest
+import flask_jwt
+
+
+class PlaceHolderTest(unittest.TestCase):
+    def test_works(self):
+        self.assertTrue(True)
